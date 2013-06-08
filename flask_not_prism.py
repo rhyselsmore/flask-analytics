9a0fce8540caffcece6b49a0a1cfd99842b653f8
Rhys Elsmore <me@rhys.io>
2013-06-08 23:20:46 +1000

    Update flask_not_prism.py

--- flask_not_prism.py
+++ flask_not_prism.py
@@ -1,2 +1,15 @@
-
 class NotPrism():
+  
+  def __init__(self, app=None):
+    
+    if app:
+      self.init_app(app)
+      
+  def dont_send_to_prism(self):
+    print "woop"
+    
+  def init_app(self, app):
+    
+    self.app = app
+    
+    app.before_request(dont_send_to_prism)
